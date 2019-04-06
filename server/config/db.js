3f011d277b22a7138d97461207c06957fdb0a099
Hinal Soni <hinalsoni20@gmail.com>
2019-04-06 19:20:21 -0400

    Initial commit

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+/*File name: app.css
+Student name: Hinal Soni
+Student ID: 300990140
+Date: March 31, 2019
+*/
+
+module.exports = {
+
+"URI" : "mongodb://hinal:hinal12345@ds155634.mlab.com:55634/comp308-assignment2",
+"secret": "SomeSecret"
+}
